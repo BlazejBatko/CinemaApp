744adb7d190d28c85d5e94025f31b5d4c6c9b80f
BlazejBatko <batkoblazej@gmail.com>
2022-06-11 17:34:46 +0200

    Api services, zmiana assetów, strona rejestracji

---
+++ CinemaApp/CinemaApp/Pages/SignupPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using CinemaApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CinemaApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignupPage : ContentPage
+    {
+        public SignupPage()
+        {
+            InitializeComponent();
+        }
+
+        private  async void ImgSignup_Tapped(object sender, EventArgs e)
+        {
+          var response = await ApiService.RegisterUser(EntName.Text, EntEmail.Text, EntPassword.Text);
+            if (response)
+            {
+                await DisplayAlert("Hej", "Konto zostało stworzone pomyślnie", "Kontynuuj");
+            }
+            else
+            {
+                await DisplayAlert(":(", "Niestety nie udało się utworzyć konta", "Anuluj");
+            }
+        }
+    }
+}
---
+++ CinemaApp/CinemaApp/Services/ApiService.cs
@@ -0,0 +1,111 @@
+﻿using CinemaApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace CinemaApp.Services
+{
+    //statyczna klasa w celu mozliwosci dostepu do zawartych funkcji w innych czesciach projektu
+    public static class ApiService
+    {
+        public static async Task<bool> RegisterUser(string name, string email, string password)
+        {
+            var register = new Register()
+            {
+                Name = name,
+                Email = email,
+                Password = password
+            };
+            //Serializacja obiektow na JSON w celu przesyłania do serwera
+            var HttpClient = new HttpClient();
+            var SerializedJson = JsonConvert.SerializeObject(register);
+            var content = new StringContent(SerializedJson, Encoding.UTF8, "application/json");
+            //asynchroniczne wysyłanie żadania do enpointu odpowiedzialnego za rejestracje użytkowników
+            //Fragment URL pochodzacy z klasy AppSettings w celu poprawy utrzymania kodu
+            var response = await HttpClient.PostAsync(AppSettings.ApiUrl + "api/users/register", content);
+            //zwracanie wartosci bool w zaleznosci od uzyskanego status code
+            if (!response.IsSuccessStatusCode) return false;
+            return true;
+        }
+
+        public async static Task<bool> Login(string email, string password) 
+        {
+            var login = new Login()
+            {
+                Email = email,
+                Password = password
+            };
+            //Serializacja obiektow na JSON w celu przesyłania do serwera
+            var HttpClient = new HttpClient();
+            var SerializedJson = JsonConvert.SerializeObject(login);
+            var content = new StringContent(SerializedJson, Encoding.UTF8, "application/json");
+            //asynchroniczne wysyłanie żadania do enpointu odpowiedzialnego za rejestracje użytkowników
+            //Fragment URL pochodzacy z klasy AppSettings w celu poprawy utrzymania kodu
+            var response = await HttpClient.PostAsync(AppSettings.ApiUrl + "api/users/login", content);
+            //zwracanie wartosci bool w zaleznosci od uzyskanego status code
+            if (!response.IsSuccessStatusCode) return false;
+            var jsonResult = await response.Content.ReadAsStringAsync();
+            //Deserializacja w oparciu o własności klasy Token
+            var result = JsonConvert.DeserializeObject<Token>(jsonResult);
+            //zapisywanie wartosci access tokenu w preferences
+            Preferences.Set("accessToken", result.access_token);
+            Preferences.Set("userId", result.user_id);
+            Preferences.Set("userName", result.user_Name);
+            return true;
+        }
+
+        public static async Task<List<Movie>> GetAllMovies(int pageNumber, int pageSize)
+        {
+            var httpClient = new HttpClient();
+            //pobieranie accessTokenu zapisanego wczesniej w Preferences
+            //Wysyłanie headera zawierającego token w celu pobrania listy filmów
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", Preferences.Get("accessToken", string.Empty));
+            var response = await httpClient.GetStringAsync(AppSettings.ApiUrl + String.Format("api/movies/AllMovies?sort=asc&pageNumber={0}&pageSize={1}", pageNumber, pageSize));
+            //Konwertowanie danych JSON na liste opartą o klasę Movie
+            return JsonConvert.DeserializeObject<List<Movie>>(response);
+        }
+
+        public static async Task<MovieDetail> GetMovieDetail(int movieId)
+        {
+            var httpClient = new HttpClient();
+            //pobieranie accessTokenu zapisanego wczesniej w Preferences
+            //Wysyłanie headera zawierającego token w celu pobrania szczegółów dotyczących filmu
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", Preferences.Get("accessToken", string.Empty));
+            var response = await httpClient.GetStringAsync(AppSettings.ApiUrl + String.Format("api/movies/MovieDetail/{0}", movieId));
+            //Konwertowanie danych JSON na liste opartą o klasę MovieDetail
+            return JsonConvert.DeserializeObject<MovieDetail>(response);
+        }
+
+        public static async Task<List<FindMovie>> FindMovies(string movieName)
+        {
+            var httpClient = new HttpClient();
+            //pobieranie accessTokenu zapisanego wczesniej w Preferences
+            //Wysyłanie headera zawierającego token w celu pobrania listy filmów zaczynających się wskazanym ciągiem znaków
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", Preferences.Get("accessToken", string.Empty));
+            var response = await httpClient.GetStringAsync(AppSettings.ApiUrl + String.Format("api/movies/FindMovies?movieName=" + movieName));
+            //Konwertowanie danych JSON na liste opartą o klasę FindMovie
+            return JsonConvert.DeserializeObject<List<FindMovie>>(response);
+        }
+
+        public static async Task<bool> ReserveMovieTicket(Reservation reservation)
+        {
+           
+            //Serializacja obiektow na JSON w celu przesyłania do serwera
+            var HttpClient = new HttpClient();
+            var SerializedJson = JsonConvert.SerializeObject(reservation);
+            var content = new StringContent(SerializedJson, Encoding.UTF8, "application/json");
+            //Header zawierający access token
+            HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", Preferences.Get("accessToken", string.Empty));
+            var response = await HttpClient.PostAsync(AppSettings.ApiUrl + "api/reservations", content);
+            //zwracanie wartosci bool w zaleznosci od uzyskanego status code
+            if (!response.IsSuccessStatusCode) return false;
+            return true;
+        }
+
+    }
+}
