0d92a9a839fb1f44a7599d4022056151101b72ea
Błażej Batko <batkoblazej@gmail.com>
2022-06-13 15:53:06 +0200

    push test

--- CinemaApp/CinemaApp/Models/FindMovie.cs
+++ CinemaApp/CinemaApp/Models/FindMovie.cs
@@ -6,6 +6,7 @@ namespace CinemaApp.Models
 {
     public class FindMovie
     {
+        //TEST TEST TEST
         public int Id { get; set; }
         public string Name { get; set; }
         public string ImageUrl { get; set; }
